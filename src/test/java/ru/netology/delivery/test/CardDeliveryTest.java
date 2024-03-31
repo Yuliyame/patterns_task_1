f4e5297bec0dc7d5f9973967543fb72746868abf
YuliyaYuliya <yr555556666667777777@gmail.com>
2024-03-31 12:47:42 +0300

    new commit

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -45,6 +45,10 @@ class CardDeliveryTest {
         $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(visible);
 
         //var validUser = DataGenerator.Registration.generateUser("ru");
         //var daysToAddForFirstMeeting = 4;
